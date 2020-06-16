4e77f1d866242de466e0598e70c925bfe605a1e8
adam-watkins <cadamrun@gmail.com>
2020-06-16 16:03:01 -0700

    deleting unneeded stuff

--- frontend/layouts/error.vue
+++ frontend/layouts/error.vue
@@ -1,14 +1,12 @@
 <template>
   <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
+
     <NuxtLink to="/">
       Home page
     </NuxtLink>
+
+    {{ error }}
+
   </v-app>
 </template>
 
@@ -20,25 +18,6 @@ export default {
       type: Object,
       default: null
     }
-  },
-  data () {
-    return {
-      pageNotFound: '404 Not Found',
-      otherError: 'An error occurred'
-    }
-  },
-  head () {
-    const title =
-      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
-    return {
-      title
-    }
   }
 }
 </script>
-
-<style scoped>
-h1 {
-  font-size: 20px;
-}
-</style>
