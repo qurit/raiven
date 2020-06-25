0941a86d400f2df33ee330554f5e0d5307ab3725
Adam <cadamrun@gmail.com>
2020-06-24 23:35:21 -0700

    testing websockets

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -9,15 +9,15 @@ export default {
     titleTemplate: '%s - ' + process.env.npm_package_name,
     title: process.env.npm_package_name || '',
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: process.env.npm_package_description || ''}
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
     ]
   },
-  loading: { color: '#B15DFF' },
+  loading: {color: '#B15DFF'},
   buildModules: [
     '@nuxtjs/vuetify',
 
@@ -26,7 +26,14 @@ export default {
     '@nuxtjs/axios',
     '@nuxtjs/auth',
     '@nuxtjs/dotenv',
+    'nuxt-socket-io'
   ],
+  io: {
+    sockets: [{
+      url: 'http://localhost:5000'
+    }]
+  },
+
 
   plugins: ['~/plugins/GlobalComponents', "~/plugins/toaster", "~/plugins/validation"],
 
