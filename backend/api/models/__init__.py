16a3f989f53294d7496f4f0eae32da8e117ad8fe
Adam <cadamrun@gmail.com>
2020-06-26 17:35:26 -0700

    building better models

--- backend/api/models/__init__.py
+++ backend/api/models/__init__.py
@@ -1,4 +1,84 @@
-from flask_restx import fields
+from bson import ObjectId
+from flask_restx import fields, Model
+
+from api import db
+
+
+def collection(cls):
+
+    def save(self):
+        return self.company
+
+    setattr(cls, '__table__', f'{cls.__name__}s')
+    setattr(cls, 'save', save)
+    return cls
+
+
+class BaseModel:
+    def __init__(self, **kwargs):
+        [self.__setattr__(k, v) for k, v in kwargs.items()]
+
+    def save(self):
+        return self.__collection__.insert_one(vars(self), {})
+
+    @classmethod
+    def model(cls, name='Base'):
+        return Model(name, {'_id': fields.String})
+
+    @classmethod
+    def list_model(cls):
+        m = cls.model()
+        return Model(f'{m.name} list', {f'{m.name}s': fields.List(fields.Nested(m))})
+
+
+@collection
+class User(BaseModel):
+
+    def __init__(self, username: str = None, name=None, title=None, department=None, company=None, last_seen=None, **kwargs):
+        super().__init__(**kwargs)
+
+        self.username = username
+        self.name = name
+        self.title = title
+        self.department = department
+        self.company = company
+        self.last_seen = last_seen
+
+    @classmethod
+    def model(cls, name='User'):
+        model = super().model(name)
+        model.update({
+            'username': fields.String,
+            'name': fields.String,
+            'title': fields.String,
+            'department': fields.String,
+            'company': fields.String,
+            'last_seen': fields.String,
+        })
+
+        return model
+
+
+class Job(BaseModel):
+
+    def __init__(self, pid=None, status=None, info=None, **kwargs):
+        super().__init__(**kwargs)
+
+        self.pid = pid
+        self.status = status
+        self.info = info
+
+    @classmethod
+    def model(cls, name='Job'):
+        model = super().model(name)
+        model.update({
+            'pid': fields.String,
+            'status': fields.String,
+            'info': fields.String,
+        })
+
+        return model
+
 
 job = (
     'Job',
@@ -23,3 +103,7 @@ user = (
     }
 )
 
+if __name__ == '__main__':
+    # job = Job('xyz', status='Queued', info='15 Second Test Func')
+    # User().save()
+    print(User().save())
--- backend/api/routes/jobs.py
+++ backend/api/routes/jobs.py
@@ -8,26 +8,27 @@ from bson.errors import InvalidId
 from api.encoders import jsonify
 from api import db, socketio
 
-from api.models import job
+from api.models import Job
 from api.tasks.hello import count_words
 
 api = Namespace('jobs', description='Background Tasks')
-job_model = api.model(*job)
-job_list_model = api.model('Job List', {'jobs': fields.List(fields.Nested(job_model))})
 
 
 @api.route('/')
 class JobsRoute(Resource):
 
-    @api.response(200, 'Ok', job_list_model)
+    @api.response(200, 'Ok', Job.list_model())
     def get(self):
         socketio.emit('my_response', {'data': 'First emit'})
         return jsonify({'jobs': db.jobs.find()})
 
-    @api.expect(job_list_model)
+    @api.expect(Job.list_model())
     def post(self):
-        msg = count_words.send()
-        _id = db.jobs.insert_one({'pid': msg.message_id, 'status': 'Queued', 'info': '15 Second Test Func'}).inserted_id
+        # msg = count_words.send()
+        job = Job('test', status='Queued', info='15 Second Test Func')
+        print('Job', vars(job))
+        _id = db.jobs.insert_one(vars(job)).inserted_id
+
         return jsonify(db.jobs.find_one({'_id': _id}))
 
     @api.response(200, 'Ok')
