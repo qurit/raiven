f4c075b4f20d01c2bbe88fe2c5c1597e20c7c94b
Kevin Lam <klam@BCCRC.CA>
2020-09-18 13:55:20 -0700

    the other dicom stuff

--- backend/api/routes/application_entity.py
+++ backend/api/routes/application_entity.py
@@ -2,10 +2,10 @@ from flask import request
 from flask_restx import Resource, Namespace, fields
 
 from api.models.application_entity import ApplicationEntity 
-# from api.models.dicom_patient import DicomPatient 
+from api.models.dicom_patient import DicomPatient 
 from api.models.dicom_store_event import DicomStoreEvent 
-# from api.models.dicom_study import DicomStudy 
-# from api.models.dicom_series import DicomSeries 
+from api.models.dicom_study import DicomStudy 
+from api.models.dicom_series import DicomSeries 
 
 api = Namespace('Application Entity', description='Dicom data related to the application entity')
 
@@ -26,24 +26,24 @@ class StoreEventRoute(Resource):
 
 # # Dicom Patient
 # # belongs to a store event
-# @api.route('/patient', methods=['GET'])
-# class DicomPatientRoute(Resource):
+@api.route('/patient', methods=['GET'])
+class DicomPatientRoute(Resource):
 
-#     def get(self):
-#         return {'dicom-patient': DicomPatient.Schema(many=True).dump(DicomPatient.query.all())}
+    def get(self):
+        return {'dicom-patient': DicomPatient.Schema(many=True).dump(DicomPatient.query.all())}
 
 # # Dicom Study
 # # belongs to a patient
-# @api.route('/study', methods=['GET'])
-# class DicomStudyRoute(Resource):
+@api.route('/study', methods=['GET'])
+class DicomStudyRoute(Resource):
 
-#     def get(self):
-#         return {'study': DicomStudy.Schema(many=True).dump(DicomStudy.query.all())}
+    def get(self):
+        return {'study': DicomStudy.Schema(many=True).dump(DicomStudy.query.all())}
 
 # # Dicom series
 # # belongs to a study
-# @api.route('/series', methods=['GET'])
-# class DicomSeriesRoute(Resource):
+@api.route('/series', methods=['GET'])
+class DicomSeriesRoute(Resource):
 
-#     def get(self):
-#         return {'series': DicomSeries.Schema(many=True).dump(DicomSeries.query.all())}
+    def get(self):
+        return {'series': DicomSeries.Schema(many=True).dump(DicomSeries.query.all())}
