251d8c6b8fdca2d1e7d779ed0851e6d0bc040b7a
Kevin Lam <klam@BCCRC.CA>
2020-09-18 10:57:38 -0700

    application entity

---
+++ backend/api/models/application_entity.py
@@ -0,0 +1,13 @@
+from sqlalchemy import *
+
+from api import db, config
+from .schemas import add_schema
+
+
+@add_schema
+class ApplicationEntity(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = Column(String)
+
+    def __repr__(self):
+        return '<ApplicationEntity {}>'.format(self.title)
