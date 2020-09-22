dd2d3a49f1bc3acd22d5385310e811001813f02e
Kevin Lam <klam@BCCRC.CA>
2020-09-22 10:11:28 -0700

    uh

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -3,7 +3,7 @@ from api import socketio, config, app as application
 if __name__ == '__main__' and not config.IS_WORKER:
     opts = {'host': config.HOST, 'port': config.PORT, 'debug': True}
 
-    if config.WEB_SOCKETS_ENABLED:
-        socketio.run(application, use_reloader=False, **opts)
-    else:
-        application.run(**opts)
+    # if config.WEB_SOCKETS_ENABLED:
+    #     socketio.run(application, use_reloader=False, **opts)
+    # else:
+    application.run(**opts)
