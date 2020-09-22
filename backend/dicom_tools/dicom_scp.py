f713e75114cc31e9511a4fcea0d88febdfdd63fc
Kevin Lam <klam@BCCRC.CA>
2020-09-22 16:15:00 -0700

    get the other stuff

--- backend/dicom_tools/dicom_scp.py
+++ backend/dicom_tools/dicom_scp.py
@@ -1,26 +1,31 @@
+import os
+
 from pynetdicom import (
     AE, debug_logger, evt, AllStoragePresentationContexts,
     ALL_TRANSFER_SYNTAXES
 )
-
-debug_logger()
+# from backend.api import db
+# debug_logger()
 
 def handle_store(event):
+    """Handle EVT_C_STORE events."""
+    print(event)
+    print(event.assoc.requestor.address)
+    print(event.assoc.requestor.port)
+    print(str(event.assoc.requestor.ae_title, encoding='utf-8').strip())
     ds = event.dataset
     ds.file_meta = event.file_meta
     ds.save_as(ds.SOPInstanceUID, write_like_original=False)
+    # print(ds)
+    print("BLAHBLAHBLAH")
+    print(ds.PatientID)
+    # TMP_DIR = os.path.join(os.getcwd(), 'tmp')
+    # path = os.path.join(TMP_DIR, ds.PatientId)
+    # if not os.path.exists(path):
+    #     os.mkdir(path)
     return 0x0000
 
-def handle_echo(event):
-    print("TESTING")
-    return 0x0000
-    
-
-handlers = [(evt.EVT_C_STORE, handle_store), (evt.EVT_C_ECHO, handle_echo)]
-
-
-ae = AE()
-storage_sop_classes = [
+handlers = [(evt.EVT_C_STORE, handle_store)]
 
 ae = AE()
 storage_sop_classes = [
