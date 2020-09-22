1f63fdb7c7ab0c3f602d11c90420f3fd6a105e26
Adam <cadamrun@gmail.com>
2020-09-22 10:59:27 -0700

    deleting old files and cleaning db

--- backend/api/database.py
+++ backend/api/database.py
@@ -1,12 +1,13 @@
 from contextlib import contextmanager
 
-from sqlalchemy import create_engine, Column, Integer
-from sqlalchemy.exc import DatabaseError
-from sqlalchemy.ext.declarative import declarative_base, declared_attr
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from inflection import underscore
 
-engine = create_engine(config.POSTGRES_URL)
+from api import config
+from api.models import BaseModel
+
+engine = create_engine(config.SQLALCHEMY_DATABASE_URI)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
@@ -17,32 +18,3 @@ def session():
         session.commit()
     finally:
         session.close()
-
-
-class BaseModel(object):
-    id = Column(Integer, primary_key=True, index=True)
-
-    @declared_attr
-    def __tablename__(self):
-        return underscore(self.__name__)
-
-    def save(self, session: Session):
-        session.add(self)
-        self._flush(session)
-        return self
-
-    def delete(self, session: Session):
-        session.delete(self)
-        self._flush(session)
-
-    # noinspection PyMethodMayBeStatic
-    def _flush(self, session: Session):
-        try:
-            session.flush()
-        except DatabaseError:
-            session.rollback()
-            raise
-
-
-BaseModel = declarative_base(cls=BaseModel)
-
--- backend/tests/test_docker.py
+++
@@ -1,5 +0,0 @@
-from tests import docker
-
-
-def test_connection():
-    assert docker.containers.list(), "Can't connect to docker daemon"
