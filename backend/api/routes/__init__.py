233e08c18567a76afe7330fc7dddb86536562d94
Adam <cadamrun@gmail.com>
2020-09-22 15:31:17 -0700

    Finished FastAPI implementation

--- backend/api/routes/__init__.py
+++ backend/api/routes/__init__.py
@@ -1,5 +1,8 @@
-from api import app
+from fastapi import Depends
+
+from api import app, session, schemas
 
 from . import application_entity
 
-app.include_router(application_entity.router)
+
+app.include_router(application_entity.router, tags=['Application Entity'], prefix='/ae')
--- backend/api/routes/application_entity.py
+++ backend/api/routes/application_entity.py
@@ -8,91 +8,21 @@ from api import session, models, schemas
 router = APIRouter()
 
 
-@router.get("/ae/", response_model=List[schemas.ApplicationEntity])
+@router.get("/", response_model=List[schemas.ApplicationEntity])
 def get_application_entities(db: Session = Depends(session)):
     return db.query(models.dicom.ApplicationEntity).all()
 
 
-@router.post("/ae/", response_model=schemas.ApplicationEntity)
+@router.post("/", response_model=schemas.ApplicationEntity)
 def create_application_entity(ae: schemas.ApplicationEntityCreate, db: Session = Depends(session)):
     return models.dicom.ApplicationEntity(title=ae.title).save(db)
 
 
-#
-# @router.post("/ae/", response_model=schemas.ApplicationEntity)
-# def add_application_entity(application_entity: schemas.ApplicationEntityCreate):
-#     print(type(application_entity))
-#     print(application_entity)
-#     db_ae = models.dicom.ApplicationEntity(**application_entity.dict()).save(db)
-#
-#     return db_ae
+@router.get("/{application_entity_id}", response_model=schemas.ApplicationEntity)
+def get_application_entity(application_entity_id: int, db: Session = Depends(session)):
+    return db.query(models.dicom.ApplicationEntity).get(application_entity_id)
 
-#
-# # Application Entity
-# @api.route('/application-entity', methods=['GET', 'POST', 'DELETE'])
-# class ApplicationEntityRoute(Resource):
-#     def get(self):
-#         return {'application_entities': ApplicationEntity.Schema(many=True).dump(ApplicationEntity.query.all())}
-#
-#     def delete(self):
-#         data = request.get_json()
-#         ApplicationEntity.query.filter(ApplicationEntity.id == data.get('id')).delete()
-#         db.session.commit()
-#         return "Application Entity deleted"
-#
-#     @api.expect(application_entity_model)
-#     def post(self):
-#         data = request.get_json()
-#         newApplicationEntity = ApplicationEntity(title=data.get('title'))
-#         db.session.add(newApplicationEntity)
-#         db.session.commit()
-#         return "Application Entity added"
-#
-# @api.route('/application-entity/<id>', methods=['GET', 'PUT', 'DELETE'])
-# class ApplicationEntityRoute(Resource):
-#     def get(self, id):
-#         return {'application-entity': ApplicationEntity.Schema(many=False).dump(ApplicationEntity.query.filter(ApplicationEntity.id == id).first())}
-#
-#     def put(self, id):
-#         data = request.get_json()
-#         newApplicationEntity = ApplicationEntity.query.filter(ApplicationEntity.id == id).first()
-#         newApplicationEntity.title = data.get('title')
-#         db.session.commit()
-#         return "Application Entity updated"
-#
-#     def delete(self, id):
-#         ApplicationEntity.query.filter(ApplicationEntity.id == id).delete()
-#         db.session.commit()
-#         return "Application Entity deleted"
-#
-# # # Dicom Store Event
-# # # belongs to an application entity
-# @api.route('/store-event', methods=['GET'])
-# class StoreEventRoute(Resource):
-#
-#     def get(self):
-#         return {'store_event': DicomStoreEvent.Schema(many=True).dump(DicomStoreEvent.query.all())}
-#
-# # # Dicom Patient
-# # # belongs to a store event
-# @api.route('/patient', methods=['GET'])
-# class DicomPatientRoute(Resource):
-#
-#     def get(self):
-#         return {'dicom-patient': DicomPatient.Schema(many=True).dump(DicomPatient.query.all())}
-#
-# # # Dicom Study
-# # # belongs to a patient
-# @api.route('/study', methods=['GET'])
-# class DicomStudyRoute(Resource):
-#
-#     def get(self):
-#         return {'study': DicomStudy.Schema(many=True).dump(DicomStudy.query.all())}
-#
-# # # Dicom series
-# # # belongs to a study
-# @api.route('/series', methods=['GET'])
-# class DicomSeriesRoute(Resource):
-#
-#     def get(self):
-#         return {'series': DicomSeries.Schema(many=True).dump(DicomSeries.query.all())}
+
+@router.delete("/{application_entity_id}", response_model=schemas.ApplicationEntity)
+def delete_application_entity(application_entity_id: int, db: Session = Depends(session)):
+    return db.query(models.dicom.ApplicationEntity).get(application_entity_id).delete(db)
