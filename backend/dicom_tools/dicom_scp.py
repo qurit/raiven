d789bb3814b72c11b3ec6ba00dd81d57bcddd267
Kevin Lam <klam@BCCRC.CA>
2020-09-23 14:48:02 -0700

    scp stuff - almost kinda working..

--- backend/dicom_tools/dicom_scp.py
+++ backend/dicom_tools/dicom_scp.py
@@ -1,24 +1,66 @@
 import os
+from sqlalchemy.orm import Session
+from fastapi import Depends
+from pydantic import BaseModel
+
+# from . import models, schemas
+
+from api import session, models, schemas
+print("FJFJSFJSFJSFJSFJSFJSFJSFJSFJSFJ")
+print(session)
+print(Depends)
 
 from pynetdicom import (
     AE, debug_logger, evt, AllStoragePresentationContexts,
     ALL_TRANSFER_SYNTAXES
 )
 # from backend.api import db
-# debug_logger()
+debug_logger()
+
+def save_ae(title, application_entity: schemas.ApplicationEntityCreate, db: Session = Depends(session)):
+    # print(db)
+    # print(ae)
+    # print(type(db))
+    # models.dicom.ApplicationEntity(title=title).save(db)
+    # db.add(dicom_store)
+    # db.commit()
+
+    newAE = models.dicom.ApplicationEntity(title=application_entity.title)
+    db.add(newAE)
+    db.commit()
+    db.refresh(newAE)
+    return newAE
 
 def handle_store(event):
     """Handle EVT_C_STORE events."""
     print(event)
     print(event.assoc.requestor.address)
     print(event.assoc.requestor.port)
-    print(str(event.assoc.requestor.ae_title, encoding='utf-8').strip())
-    ds = event.dataset
-    ds.file_meta = event.file_meta
-    ds.save_as(ds.SOPInstanceUID, write_like_original=False)
+    title = str(event.assoc.requestor.ae_title, encoding='utf-8').strip()
+    newAE = models.dicom.ApplicationEntity(title=title)
+    session.add(newAE)
+    session.commit()
+    print(newAE.title)
+    print(newAE)
+    print(db)
+    print(type(db))
+    
+    # db.add(newAE)
+    # application_entity = schemas.ApplicationEntityCreate()
+    # application_entity.title = title
+    # save_ae(title, newAE)
+
+    # dicom_store = models.dicom.ApplicationEntityCreate(title=title)
+    # db.add(dicom_store)
+    # db.commit()
+
+    # ds = event.dataset
+    # ds.file_meta = event.file_meta
+    # ds.save_as(ds.SOPInstanceUID, write_like_original=False)
+    # models.dicom.ApplicationEntity(title)
     # print(ds)
     print("BLAHBLAHBLAH")
-    print(ds.PatientID)
+    # print(ds.PatientID)
     # TMP_DIR = os.path.join(os.getcwd(), 'tmp')
     # path = os.path.join(TMP_DIR, ds.PatientId)
     # if not os.path.exists(path):
