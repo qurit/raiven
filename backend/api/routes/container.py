19c99d9961262b9ad24f302b20bd02a80438a210
Adam <cadamrun@gmail.com>
2020-09-24 11:48:34 -0700

    routes finished

--- backend/api/routes/container.py
+++ backend/api/routes/container.py
@@ -3,30 +3,27 @@ from typing import List
 from sqlalchemy.orm import Session
 from fastapi import APIRouter, Depends
 
-from api import session, models, schemas
+from api import session, schemas
+from api.models.pipeline import Container
+
 router = APIRouter()
 
 
-@router.get("/", response_model=List[schemas.Pipeline])
-def get_all_pipelines(db: Session = Depends(session)):
-    return db.query(models.pipeline.Pipeline).all()
+@router.get("/", response_model=List[schemas.Container])
+def get_all_containers(db: Session = Depends(session)):
+    return db.query(Container).all()
 
 
-@router.post("/", response_model=schemas.Pipeline)
-def create_pipeline(pipeline: schemas.PipelineCreate, db: Session = Depends(session)):
-    return models.pipeline.Pipeline(**pipeline.dict()).save(db)
+@router.post("/", response_model=schemas.Container)
+def create_container(container: schemas.ContainerCreate, db: Session = Depends(session)):
+    return Container(**container.dict()).save(db)
 
 
-@router.get("/{pipeline_id}", response_model=schemas.Pipeline)
-def get_pipeline(pipeline_id: int, db: Session = Depends(session)):
-    return db.query(models.pipeline.Pipeline).get(pipeline_id)
+@router.get("/{container_id}", response_model=schemas.Container)
+def get_container(container_id: int, db: Session = Depends(session)):
+    return db.query(Container).get(container_id)
 
 
-@router.delete("/{pipeline_id}", response_model=schemas.Pipeline)
-def delete_pipeline(pipeline_id: int, db: Session = Depends(session)):
-    return db.query(models.pipeline.Pipeline).get(pipeline_id).delete(db)
-
-
-@router.post("/{pipeline_id}/steps", response_model=schemas.Pipeline)
-def create_pipeline_step(step: schemas.PipelineStepCreate, db: Session = Depends(session)):
-    return models.pipeline.PipelineStep(**step.dict()).save(db)
+@router.delete("/{container_id}", response_model=schemas.Pipeline)
+def delete_container(container_id: int, db: Session = Depends(session)):
+    return db.query(Container).get(container_id).delete(db)
