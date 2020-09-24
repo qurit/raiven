76e79fbc31c1096849722c5a6169c26e6f3c4bc9
Adam <cadamrun@gmail.com>
2020-09-24 11:27:08 -0700

    pipeline crud

--- backend/api/routes/__init__.py
+++ backend/api/routes/__init__.py
@@ -2,8 +2,9 @@ from fastapi import Depends
 
 from api import app, session, schemas
 
-from . import application_entity, pipeline
+from . import application_entity, pipeline, container
 
 
 app.include_router(application_entity.router, tags=['Application Entity'], prefix='/ae')
 app.include_router(pipeline.router, tags=['Pipeline'], prefix='/pipeline')
+app.include_router(container.router, tags=['Container'], prefix='/container')
--- backend/api/routes/container.py
+++ backend/api/routes/container.py
@@ -3,9 +3,7 @@ from typing import List
 from sqlalchemy.orm import Session
 from fastapi import APIRouter, Depends
 
-from api import session, schemas
-from api.models.pipeline import Pipeline, PipelineStep
-
+from api import session, models, schemas
 router = APIRouter()
 
 
@@ -16,19 +14,19 @@ def get_all_pipelines(db: Session = Depends(session)):
 
 @router.post("/", response_model=schemas.Pipeline)
 def create_pipeline(pipeline: schemas.PipelineCreate, db: Session = Depends(session)):
-    return Pipeline(**pipeline.dict()).save(db)
+    return models.pipeline.Pipeline(**pipeline.dict()).save(db)
 
 
 @router.get("/{pipeline_id}", response_model=schemas.Pipeline)
 def get_pipeline(pipeline_id: int, db: Session = Depends(session)):
-    return db.query(Pipeline).get(pipeline_id)
+    return db.query(models.pipeline.Pipeline).get(pipeline_id)
 
 
 @router.delete("/{pipeline_id}", response_model=schemas.Pipeline)
 def delete_pipeline(pipeline_id: int, db: Session = Depends(session)):
-    return db.query(Pipeline).get(pipeline_id).delete(db)
+    return db.query(models.pipeline.Pipeline).get(pipeline_id).delete(db)
 
 
 @router.post("/{pipeline_id}/steps", response_model=schemas.Pipeline)
 def create_pipeline_step(step: schemas.PipelineStepCreate, db: Session = Depends(session)):
-    return PipelineStep(**step.dict()).save(db)
+    return models.pipeline.PipelineStep(**step.dict()).save(db)
