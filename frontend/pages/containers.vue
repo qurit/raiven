fcc0b59609c18c030cd70383d782f19d1a7f39a1
KlamChowder1 <kevin.k.f.lam1@gmail.com>
2020-09-28 14:12:12 -0700

    some container/pipeline store stuff, need to do routes and dashboard view

---
+++ frontend/pages/containers.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <ContainerList />
+  </div>
+</template>
+
+<script>
+import ContainerList from '../components/ContainerList'
+export default {
+  components: { ContainerList }
+}
+</script>
