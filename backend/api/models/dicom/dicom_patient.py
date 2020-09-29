a798f47eff520152e9ea617b631271712c850862
Kevin Lam <klam@BCCRC.CA>
2020-09-29 15:44:53 -0700

    halfdone SCP rip

--- backend/api/models/dicom/dicom_patient.py
+++ backend/api/models/dicom/dicom_patient.py
@@ -5,10 +5,10 @@ from .. import Base
 
 
 class DicomPatient(Base):
-    id = Column(Integer, primary_key=True)
-    dicom_store_event_id = Column(Integer, ForeignKey("dicom_store_event.id", ondelete="CASCADE"))
+    id = Column(String, primary_key=True)
+    # dicom_store_event_id = Column(Integer, ForeignKey("dicom_store_event.id", ondelete="CASCADE"))
 
-    dicom_store_event = relationship('DicomStoreEvent')
+    # dicom_store_event = relationship('DicomStoreEvent')
 
     def __repr__(self):
         return '<DicomPatient {}>'.format(self.id)
