13e1459f6a0392a3f1d9b4c51a3d72ce9f35c369
KlamChowder1 <kevin.k.f.lam1@gmail.com>
2020-10-07 10:59:55 -0700

    saving container to file works but need to clean

--- backend/api/routes/container.py
+++ backend/api/routes/container.py
@@ -18,8 +18,38 @@ def get_all_containers(db: Session = Depends(session)):
     return db.query(Container).all()
 
 
-@router.post("/", response_model=container.Container)
-def create_container(container: container.ContainerCreate, db: Session = Depends(session)):
+@router.post("/")
+async def create_container(file: bytes = File(...), filename: str = Form(...), description: str = Form(...), db: session = Depends(session)):
+    print('in here')
+    # print(file)
+    # print(filename)
+    # print(description)
+
+    save_path = 'C:\\Users\\kevin\\Desktop\\picom\\backend\\user_files'
+    # TODO: this gave me a "permission denied error" when trying to write to file
+    # with open(save_path) as f:
+    #     f.write(file)
+    completeFile = os.path.join(save_path, filename)
+    file1 = open(completeFile, "wb")
+    file1.write(file)
+    file1.close()
+
+    new_container = container.ContainerCreate(user_id=1, name=filename, description=description,
+                                              dockerfile_path=completeFile, is_input_container=False, is_output_container=False)
+    print(new_container)
+    db_container = Container(**new_container.dict())
+    db_container.save(db)
+
+    return db_container
+    # db_assignment = db.query(models.Assignment).get(assignment_id)
+    # if not os.path.exists(assignment_folder := db_assignment.path):
+    #     os.mkdir(assignment_folder)
+    # with open('tmp', 'wb') as f:
+    #     f.write(file)
+    # shutil.unpack_archive('tmp', extract_dir=assignment_folder, format='zip')
+
+# @router.post("/", response_model=container.Container)
+# def create_container(container: container.ContainerCreate, db: Session = Depends(session)):
     """ Allows the creation of a new container """
 
     # dockerfile_path = os.path.join(container.get_path(), 'dockerfile')
@@ -47,23 +77,10 @@ def create_container(container: container.ContainerCreate, db: Session = Depends
     # with open(dockerfile_path) as fp:
     #     fp.write(container.dockerfile)
 
-    db_container = Container(**container.dict())
-    db_container.save(db)
+    # db_container = Container(**container.dict())
+    # db_container.save(db)
 
-    return db_container
-
-
-# @router.post("/{container_id}", response_model=schemas.Container)
-# def upload_dockerfile(container_id: int, dockerfile: bytes = File(...), db: Session = Depends(session)):
-#     """ Allows the upload of dockerfile to be used with a container """
-#     container = db.query(Container).get(container_id)
-#     dockerfile_path = os.path.join(container.get_path(), 'dockerfile')
-
-#     with open(dockerfile_path) as fp:
-#         fp.write(dockerfile)
-#         fp.close()
-
-#     return container
+    # return db_container
 
 
 @router.get("/{container_id}", response_model=container.Container)
--- frontend/components/ContainerForm.vue
+++ frontend/components/ContainerForm.vue
@@ -56,22 +56,32 @@ export default {
     }
   },
   methods: {
-    updateDockerFile(file) {
-      console.log(file)
-      // const test = JSON.stringify(file)
-      // console.log(typeof file)
-      // this.dockerFileName = file.name
+    readFile(file) {
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader()
+        reader.onload = () => resolve(reader.result)
+        reader.onerror = reject
+        reader.readAsArrayBuffer(file)
+      })
+    },
+    async updateDockerFile(file) {
+      const config = { headers: { 'Content-Type': 'multipart/form-data' } }
+      const f = await this.readFile(file)
+      const formData = new FormData()
+      formData.append('name', this.name)
+      formData.append('dockerfile_path', 'blah')
+      formData.append('description', this.description)
+      formData.append('is_input_container', this.input)
+      formData.append('is_output_container', this.output)
+      formData.append('filename', file.name)
+      formData.append('file', new Blob([f]))
+      for (var pair of formData.entries()) {
+        console.log(pair[0] + ', ' + pair[1])
+      }
 
-      // const testing = new Blob([JSON.stringify(file)], {
-      //   type: 'application/json'
-      // })
-
-      // console.log(testing)
-
-      // const reader = new FileReader()
-      // const test = reader.readAsText(testing, 'UTF-8')
-
-      this.file = file
+      console.log('got in here')
+      const path = 'http://localhost:5000/container'
+      await axios.post(path, formData)
     },
     addContainer(payload) {
       console.log(payload)
