ebc30f9dd18a0806496b5dd3463e43900cf2ca89
KlamChowder1 <kevin.k.f.lam1@gmail.com>
2020-10-07 22:50:00 -0700

    fixed optional description thing

--- backend/api/schemas/container.py
+++ backend/api/schemas/container.py
@@ -1,10 +1,11 @@
 from . import BaseModel, BaseORMModel
+from typing import Optional
 
 
 class ContainerCreate(BaseModel):
     user_id: int
     name: str
-    description: str
+    description: Optional[str] = None
     dockerfile_path: str
     is_input_container: bool
     is_output_container: bool
