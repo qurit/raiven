53d6f6243fee94fadeb5e5d14ea72e44e29effcd
Adam <cadamrun@gmail.com>
2020-10-15 11:46:28 -0700

    almost done container code

--- backend/api/models/pipeline.py
+++ backend/api/models/pipeline.py
@@ -52,8 +52,13 @@ class PipelineJob(PathMixin, Base):
     pipeline_run_id = Column(ForeignKey("pipeline_run.id", ondelete="CASCADE"))
     pipeline_node_id = Column(ForeignKey("pipeline_node.id", ondelete="CASCADE"))
     status = Column(String)
+    exit_code = Column(Integer)
+    # TODO: Add Error logging (seperate table)
+
     input_path = Column(String)
     output_path = Column(String)
+
+    error = relationship('PipelineJobError', backref="job", uselist=False)
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
@@ -67,3 +72,8 @@ class PipelineJob(PathMixin, Base):
         self.input_path = os.path.join(rel_path, INPUT_DIRNAME)
         self.output_path = os.path.join(rel_path, OUTPUT_DIRNAME)
         super().save(*args, **kwargs)
+
+
+class PipelineJobError(Base):
+    pipeline_job_id = Column(ForeignKey("pipeline_job.id", ondelete="CASCADE"))
+    stderr = Column(String)
--- backend/api/pipelining/__init__.py
+++ backend/api/pipelining/__init__.py
@@ -1,15 +1,50 @@
+import os
+import shutil
 import docker
 from api import config, worker_session, models
 
+client = docker.from_env()
 
-def run_node(db, run_id: int, node_id: int, previous_job_id: int = None):
+
+def run_node(run_id: int, node_id: int, previous_job_id: int = None):
     print(f"RUNNING NODE: {node_id}, RUN: {run_id}")
 
-    job = models.pipeline.PipelineJob(
-        pipeline_run_id=run_id,
-        pipeline_node_id=node_id,
-        status='Created',
-    ).save(db)
+    with worker_session() as db:
+        job = models.pipeline.PipelineJob(pipeline_run_id=run_id, pipeline_node_id=node_id, status='Created')
+        job.save(db)
+        job.detach(db)
+
+    # TODO: Locking
+    if not previous_job_id:
+        shutil.copytree('C:\\Users\\Adam\\Programming\\picom\\examples\\input', job.to_abs_path(job.input_path), dirs_exist_ok=True)
+    else:
+        # TODO: COPY files from previous job
+        pass
+
+    volumes = {
+        job.to_abs_path(job.input_path): {'bind': config.PICOM_INPUT_DIR, 'mode': 'ro'},
+        job.to_abs_path(job.output_path): {'bind': config.PICOM_OUTPUT_DIR, 'mode': 'ro'}
+    }
+
+    container: Container = client.containers.run('picom_example_container', detach=True, volumes=volumes)
+    with worker_session() as db:
+        job.status = 'running'
+        job.save(db)
+        job.detach(db)
+
+    # Long running task
+    exit_code = container.wait()['StatusCode']
+
+    with worker_session() as db:
+        job.status = 'exited'
+        job.exit_code = exit_code
+        job.save(db)
+
+        if exit_code != 0:
+            container.reload()
+            stderr = container.logs(stdout=False, stderr=True).decode("utf-8")
+            job_error = models.pipeline.PipelineJobError(pipeline_job_id=job.id, stderr=stderr)
+            job_error.save(db)
 
 
 def run_pipeline(folder: str, pipeline_id: int):
@@ -18,10 +53,10 @@ def run_pipeline(folder: str, pipeline_id: int):
         starting_nodes = pipeline.get_starting_nodes()
 
         run = models.pipeline.PipelineRun().save(db)
+        db.commit()
 
-        [run_node(db, run.id, n.id) for n in starting_nodes]
+        [run_node(run.id, n.id) for n in starting_nodes]
 
-# client = docker.from_env()
 
 if __name__ == '__main__':
     run_pipeline('xyz', 1)
