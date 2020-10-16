92e4688006dc21e7f28ba39491aad3ab638171aa
KlamChowder1 <kevin.k.f.lam1@gmail.com>
2020-10-16 16:10:32 -0700

    more output destination stuff

--- frontend/components/OutputDestinationForm.vue
+++ frontend/components/OutputDestinationForm.vue
@@ -5,7 +5,7 @@
       <v-row>
         <v-col cols="6">
           <v-text-field
-            v-model="hostAddressName"
+            v-model="host"
             label="Host Address"
             :rules="[v => !!v || 'A Host Address is required']"
             required
@@ -13,7 +13,7 @@
         </v-col>
         <v-col cols="6">
           <v-text-field
-            v-model="portName"
+            v-model="port"
             label="Port"
             :rules="[v => !!v || 'A Port is required']"
             required
@@ -22,9 +22,30 @@
       </v-row>
     </v-form>
     <v-row justify="center">
-      <v-btn class="ma-4" color="green">
+      <v-btn class="ma-4" color="green" @click="submit">
         Add
       </v-btn>
     </v-row>
   </v-card>
 </template>
+
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      host: '',
+      port: null
+    }
+  },
+  methods: {
+    async submit() {
+      this.$store.dispatch('destination/addDestination', {
+        host: this.host,
+        port: this.port
+      })
+      this.$emit('closeDialog')
+    }
+  }
+}
+</script>
