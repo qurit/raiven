6ad036ae91577d3400cd2fb8fcc1712366657c21
Adam <cadamrun@gmail.com>
2020-10-20 00:31:15 -0700

    refactored instance form and tied into backend

--- frontend/api/index.js
+++ frontend/api/index.js
@@ -2,6 +2,8 @@ export const generic_get = async ({ $axios }, url, params) => (await $axios.get(
 
 export const generic_post = async ({ $axios }, url, payload) => (await $axios.post(url, payload)).data
 
+export const generic_put = async ({ $axios }, url, payload) => (await $axios.put(url, payload)).data
+
 export const generic_delete = async ({ $axios }, url) => (await $axios.delete(url)).data
 
 
