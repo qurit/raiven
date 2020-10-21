88f827e99f2616e7651977ba0abfd07492e69c99
KlamChowder1 <kevin.k.f.lam1@gmail.com>
2020-10-21 13:13:53 -0700

    small cleanup

--- frontend/components/container/ContainerCard.vue
+++ frontend/components/container/ContainerCard.vue
@@ -2,21 +2,27 @@
   <v-hover v-slot:default="{ hover }">
     <v-card
       class="ma-2 title"
-      :color="hover ? containerColor + ' lighten-1' : containerColor"
-      :elevation="hover ? 12:0"
+      :color="hover ? 'blue' + ' lighten-1' : 'blue'"
+      :elevation="hover ? 12 : 0"
       style="transition: background-color 0.2s ease-out"
     >
-        <v-card-title v-text="container.name"/>
-        <v-card-subtitle v-text="`ID: ${container.id}`" class="pb-4" />
-        <v-card-text>
-            {{ container.description }}
-        </v-card-text>
-        <v-card-actions>
-          <v-chip small v-for="chip in chips" v-text="chip" class="mr-1" />
-          <v-spacer />
-          <!-- Slot to enable adding of custom actions -->
-          <slot></slot>
-        </v-card-actions>
+      <v-card-title v-text="container.name" />
+      <v-card-subtitle v-text="`ID: ${container.id}`" class="pb-4" />
+      <v-card-text>
+        {{ container.description }}
+      </v-card-text>
+      <v-card-actions>
+        <v-chip
+          small
+          v-for="chip in chips"
+          v-text="chip"
+          class="mr-1"
+          :color="containerColor"
+        />
+        <v-spacer />
+        <!-- Slot to enable adding of custom actions -->
+        <slot></slot>
+      </v-card-actions>
     </v-card>
   </v-hover>
 </template>
@@ -24,18 +30,18 @@
 export default {
   name: 'ContainerCard',
   props: {
-      container: {
-        type: Object,
-        default: () => undefined
-      },
-      colors: {
-        type: Object,
-        default: () => ({
-            input: 'orange',
-            output: 'purple',
-            default: 'blue'
-        })
-      }
+    container: {
+      type: Object,
+      default: () => undefined
+    },
+    colors: {
+      type: Object,
+      default: () => ({
+        input: 'orange',
+        output: 'purple',
+        default: 'blue'
+      })
+    }
   },
   computed: {
     containerColor: ctx => {
