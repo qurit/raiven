b740458d87f5399e79e866e6291243a57cdccfca
Adam <cadamrun@gmail.com>
2020-10-21 18:35:12 -0700

    raivent styling

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -1,8 +1,16 @@
 <template>
-  <v-row justify="center" align="center" style="height: inherit">
+  <v-row justify="center" align="center" style="height: inherit" no-gutters>
     <v-col cols="12">
-      <div class="display-3 text-center">CRC Imaging Pipeline</div>
-      <div class="mx-auto mt-8" style="max-width: 500px">
+      <div class="text-center display-4 raiven" style="font-family: 'Monoton', sans-serif !important;" v-html="title" />
+      <VueTypedJs
+        :typeSpeed="25"
+        :showCursor="false"
+        :strings="[subtitle]"
+        @onComplete="complete = true">
+        <div class="typing text-center title raiven"  style="width: 100%;"></div>
+      </VueTypedJs>
+      <v-expand-transition>
+        <v-card class="mx-auto mt-8 pa-4" style="max-width: 500px; border-radius: 24px" v-if="complete">
         <v-text-field
           v-model="username"
           :error="error"
@@ -11,6 +19,7 @@
           type="text"
           solo
           flat
+          rounded
         />
         <v-text-field
           v-model="password"
@@ -19,28 +28,39 @@
           label="Password"
           prepend-inner-icon="mdi-lock"
           type="password"
-          solo
           flat
+          solo
+          rounded
         />
         <v-row no-gutters>
           <v-spacer />
-          <v-btn @click="login" rounded color="primary" light class="px-4">
+          <v-btn :to="'/'" @click="login" rounded color="primary secondary--text" light class="px-4">
             <v-icon v-text="'mdi-lock'" />
             Login
           </v-btn>
         </v-row>
-      </div>
+      </v-card>
+      </v-expand-transition>
     </v-col>
+
+<!-- qurit logo   -->
+    <v-img src="qurit-logo.png" max-width="150px" contain id="qurit" class="pa-4" />
   </v-row>
 </template>
 
 <script>
+import { VueTypedJs } from 'vue-typed-js'
 export default {
   name: 'login',
   layout: 'fullHeight',
+  components: {VueTypedJs},
   data: () => ({
     username: '',
-    password: ''
+    password: '',
+    complete: false,
+    title: 'R<span>AI</span>VEN',
+    subtitle: 'The <span>AI</span> Radiology Environment of the Future'
+
   }),
   methods: {
     login() {
@@ -49,3 +69,22 @@ export default {
   }
 }
 </script>
+<style lang="scss">
+  .title,
+  .display-4 {
+    color: #33333d;
+  }
+
+  .raiven > span {
+    color: var(--v-primary-base)
+  }
+
+  .display-4 {
+    transition: color 5s linear;
+  }
+
+  #qurit {
+    position:fixed;
+    bottom:0;
+  }
+</style>
