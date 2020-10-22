3d32c745755d1aedb6e7138e8ac06e512f95e9eb
KlamChowder1 <kevin.k.f.lam1@gmail.com>
2020-10-21 21:36:05 -0700

    this seems pretty convoluted..

--- frontend/components/dicom/DicomInstances.vue
+++ frontend/components/dicom/DicomInstances.vue
@@ -32,40 +32,37 @@
 
         <!-- Patients -->
         <!-- NEED TO FIX THE THING.. SEEMS LIKE ITS BECAUSE ITS AUTO OPENED? OR SOMETHING? -->
-        <div v-if="dicomNodes[dicomEvent.id] !== undefined">
+        <div v-if="!!dicomNodes[dicomEvent.id]">
           <v-list-item
-            v-for="dicomPatient in dicomNodes[dicomEvent.id].patients"
-            :key="dicomPatient.id"
+            v-for="dicomPatient in Object.entries(dicomNodes[dicomEvent.id])"
+            :key="dicomPatient[0]"
           >
             <v-list-group
               no-action
               sub-group
               :ripple="false"
-              @click="loadStudyContent"
+              @click="loadStudyContent(dicomEvent.id, dicomPatient[0])"
             >
               <v-btn
                 color="blue"
                 small
                 :ripple="false"
                 class="ml-16"
-                @click="send('patient', dicomPatient.id)"
+                @click="send('patient', dicomPatient)"
               >
-                Send Patient {{ dicomPatient.id }} to a Pipeline
+                Send Patient {{ dicomPatient[0] }} to a Pipeline
               </v-btn>
               <template v-slot:activator>
                 <v-list-item-content>
                   <v-list-item-title
-                    >{{ dicomPatient.id }} Patient
-                    {{ dicomPatient.patient_id }}</v-list-item-title
+                    >{{ dicomPatient[0] }} Patient
+                    {{ dicomPatient[1] }}</v-list-item-title
                   >
                 </v-list-item-content>
               </template>
 
               <!-- Studies -->
-              <v-list-item
-                v-for="dicomStudy in dicomPatient.studies"
-                :key="dicomStudy.id"
-              >
+              <v-list-item v-for="dicomStudy in [1, 2]" :key="dicomStudy.id">
                 <v-list-group
                   no-action
                   sub-group
@@ -91,7 +88,7 @@
                   </template>
 
                   <!-- Series -->
-                  <v-list-item
+                  <!-- <v-list-item
                     v-for="dicomSeries in dicomStudy.series"
                     :key="dicomSeries.id"
                   >
@@ -105,7 +102,7 @@
                     >
                       Send Series {{ dicomSeries.id }} to a Pipeline
                     </v-btn>
-                  </v-list-item>
+                  </v-list-item> -->
                 </v-list-group>
               </v-list-item>
             </v-list-group>
@@ -148,15 +145,43 @@ export default {
     async loadPatientContent(dicomNodeId) {
       const URL = `/dicom/nodes/${dicomNodeId}/patients`
       const res = await generic_get(this, URL)
-      this.dicomNodes = { [dicomNodeId]: { patients: res } }
+      console.log(res)
+      res.forEach(patient => {
+        this.dicomNodes = {
+          [dicomNodeId]: {
+            [patient.id]: patient.patient_id
+          }
+        }
+      })
       console.log(this.dicomNodes)
+      console.log(Object.entries(this.dicomNodes[dicomNodeId]))
     },
-    loadStudyContent(dicomNodeId) {
-      // const URL = `/dicom/nodes/${dicomNodeId}/patient/${patientId}`
-      // const res = await generic_get(this, URL)
-      // this.dicomNodes = {[dicomNodeId] : {
-      //   patient[patientId]
-      // }}
+    async loadStudyContent(dicomNodeId, patientId) {
+      console.log(dicomNodeId)
+      console.log(patientId)
+      const URL = `/dicom/nodes/${dicomNodeId}/patient/${patientId}/studies`
+      const res = await generic_get(this, URL)
+
+      console.log(res)
+
+      res.forEach(study => {
+        this.dicomNodes = {
+          [dicomNodeId]: {
+            [patientId]: {
+              [study.id]: {
+                dicomPatientId: study.dicom_patient_id,
+                studyDate: study.study_date,
+                studyInstanceUid: study.study_instance_uid
+              }
+            }
+          }
+        }
+      })
+      console.log(this.dicomNodes)
+      // console.log(res)
+      // const studies = { [patientId]: { studies: res } }
+      // this.dicomNodes
+      // console.log(this.dicomNodes)
     },
     loadSeriesContent() {
       console.log('clicked study accordion')
