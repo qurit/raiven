2f07b0b594b5795564bd4917fce7af56a5383299
Kevin <kevin.k.f.lam1@gmail.com>
2020-11-02 16:40:57 -0800

    trying with the nuxt package but everything is still broken

--- backend/api/routes/dicom.py
+++ backend/api/routes/dicom.py
@@ -1,10 +1,10 @@
 import os
 
-from fastapi import FastAPI
+
+from fastapi import FastAPI, WebSocket
 from sqlalchemy.orm import Session
 from fastapi import APIRouter, Depends
 from typing import List
-from fastapi.websockets import WebSocket
 
 from api import session
 from api.schemas import dicom, pipeline
@@ -14,21 +14,23 @@ from api.models.dicom import DicomNode, DicomPatient, DicomSeries, DicomStudy
 router = APIRouter()
 app = FastAPI()
 
+print(app.websocket)
 
-@app.websocket_route("/test")
+
+@app.websocket("/test")
 async def test(websocket: WebSocket):
+    print("hello")
     await websocket.accept()
     await websocket.send_json({"msg": "Hello WebSocket"})
     await websocket.close()
 
 
-@router.get("/nodes", response_model=List[dicom.DicomNode])
+@ router.get("/nodes", response_model=List[dicom.DicomNode])
 def get_all_dicom_nodes(db: Session = Depends(session)):
-    print("Here")
     return db.query(DicomNode).all()
 
 
-@router.put("/node/{dicom_node_id}")
+@ router.put("/node/{dicom_node_id}")
 def send_dicom_node(dicom_node_id: int, pipeline_id: pipeline.PipelineId, db: Session = Depends(session)):
     print(dicom_node_id)
     print(pipeline_id)
@@ -36,19 +38,19 @@ def send_dicom_node(dicom_node_id: int, pipeline_id: pipeline.PipelineId, db: Se
     return(dicom_node.abs_path)
 
 
-@router.put("/node/{dicom_node_id}/{dicom_patient_id}")
+@ router.put("/node/{dicom_node_id}/{dicom_patient_id}")
 def send_dicom_patient(dicom_node_id: int, dicom_patient_id: int, pipeline_id: pipeline.PipelineId, db: Session = Depends(session)):
     dicom_patient = db.query(DicomPatient).get(dicom_patient_id)
     return(dicom_patient.abs_path)
 
 
-@router.put("/node/{dicom_node_id}/{dicom_patient_id}/{dicom_study_id}")
+@ router.put("/node/{dicom_node_id}/{dicom_patient_id}/{dicom_study_id}")
 def send_dicom_study(dicom_node_id: int, dicom_patient_id: int, dicom_study_id: int,   pipeline_id: pipeline.PipelineId, db: Session = Depends(session)):
     dicom_study = db.query(DicomStudy).get(dicom_study_id)
     return(dicom_study.abs_path)
 
 
-@router.put("/node/{dicom_node_id}/{dicom_patient_id}/{dicom_study_id}/{dicom_series_id}")
+@ router.put("/node/{dicom_node_id}/{dicom_patient_id}/{dicom_study_id}/{dicom_series_id}")
 def send_dicom_series(dicom_node_id: int, dicom_patient_id: int, dicom_study_id: int, dicom_series_id: int,  pipeline_id: pipeline.PipelineId, db: Session = Depends(session)):
     dicom_series = db.query(DicomSeries).get(dicom_series_id)
     return(dicom_series.abs_path)
--- frontend/components/WebSocketTest.vue
+++ frontend/components/WebSocketTest.vue
@@ -12,7 +12,8 @@ export default {
   data() {
     return {
       connection: null,
-      test: 'hello world'
+      test: 'hello world',
+      socketResponse: undefined
     }
   },
   sockets: {
@@ -24,19 +25,32 @@ export default {
     sendMessage(message) {
       console.log(this.connection)
       this.connection.send(message)
+    },
+    getMessage() {
+      this.socket.emit('getMessage', { data: 'testing' }, res => {
+        this.socketResponse = res
+      })
     }
   },
   created() {
-    console.log('Starting Connection')
-    // this.connection = new WebSocket('wss://echo.websocket.org')
-    this.connection = new WebSocket('wss://localhost:5000/dicom/test')
-    console.log(this.connection)
-    this.connection.onopen = function(event) {
-      console.log('connected!!!!')
-    }
-    this.connection.onmessage = function(event) {
-      console.log(event)
-    }
+    // try with nuxt websocket package
+    this.socket = this.$nuxtSocket({
+      name: 'test',
+      channel: '/dicom/test'
+    })
+
+    // try with mdn websockets
+    // console.log('Starting Connection')
+    // // this.connection = new WebSocket('wss://echo.websocket.org')
+    // this.connection = new WebSocket('wss://localhost:5000/dicom/test')
+    // console.log(this.connection)
+    // console.log(this.connection.readyState)
+    // this.connection.onopen = function(event) {
+    //   console.log('connected!!!!')
+    // }
+    // this.connection.onmessage = function(event) {
+    //   console.log(event)
+    // }
   }
 }
 </script>
