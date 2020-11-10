ea698b02b6829aacd625bae139129fb42e7bc49e
Kevin <kevin.k.f.lam1@gmail.com>
2020-11-09 16:57:09 -0800

    backend works on fastapi docs but frontend doesnt

--- backend/api/routes/pipeline.py
+++ backend/api/routes/pipeline.py
@@ -1,12 +1,16 @@
-from typing import List
-
-from sqlalchemy.orm import Session
-from fastapi import APIRouter, Depends, BackgroundTasks
-
-from api import session
-from api.models.pipeline import Pipeline, PipelineLink, PipelineNode, PipelineRun
-from api.controllers.pipeline import PipelineController
 from api.schemas import pipeline as schemas
+from api.controllers.pipeline import PipelineController
+from api.models.pipeline import Pipeline, PipelineLink, PipelineNode, PipelineRun
+from api import session
+from fastapi import APIRouter, Depends, BackgroundTasks
+from sqlalchemy.orm import Session
+from typing import List
+from fastapi.responses import StreamingResponse
+
+import os
+import zipfile
+import io
+
 
 router = APIRouter()
 
@@ -16,24 +20,55 @@ def get_all_pipeline_runs(db: Session = Depends(session)):
     return db.query(PipelineRun).all()
 
 
-@router.get("/", response_model=List[schemas.Pipeline])
+@router.get("/download/{pipeline_run_id}")
+def download_pipeline_run(pipeline_run_id: int, db: Session = Depends(session)):
+    print(pipeline_run_id)
+
+    pipeline_run = db.query(
+        PipelineRun).get(pipeline_run_id)
+
+    result_path = os.path.join(
+        pipeline_run.get_abs_path(), "output")
+
+    result_files = (os.listdir(os.path.join(
+        pipeline_run.get_abs_path(), "output")))
+
+    compression = zipfile.ZIP_DEFLATED
+    zf = zipfile.ZipFile("RAWS.zip", mode="w")
+    for file_name in result_files:
+        zf.write(os.path.join(result_path, file_name),
+                 file_name, compress_type=compression)
+
+    zf.close()
+
+    zip_file = open("RAWS.zip", 'rb')
+    response = StreamingResponse(
+        zip_file, media_type="application/x-zip-compressed")
+    response.headers["Content-Disposition"] = "attachment; filename=test.zip"
+    print(response)
+    return response
+
+# print(pipeline_run_result_path)
+
+
+@ router.get("/", response_model=List[schemas.Pipeline])
 def get_all_pipelines(db: Session = Depends(session)):
     return db.query(Pipeline).all()
 
 
-@router.post("/", response_model=schemas.Pipeline)
+@ router.post("/", response_model=schemas.Pipeline)
 def create_pipeline(pipeline: schemas.PipelineCreate, db: Session = Depends(session)):
     print("got here")
     print(pipeline)
     return Pipeline(**pipeline.dict()).save(db)
 
 
-@router.get("/{pipeline_id}", response_model=schemas.PipelineFull)
+@ router.get("/{pipeline_id}", response_model=schemas.PipelineFull)
 def get_pipeline(pipeline_id: int, db: Session = Depends(session)):
     return db.query(Pipeline).get(pipeline_id)
 
 
-@router.put("/{pipeline_id}", response_model=schemas.PipelineRun)
+@ router.put("/{pipeline_id}", response_model=schemas.PipelineRun)
 def run_pipeline(pipeline_id: int, run_options: schemas.PipelineRunOptions, background_tasks: BackgroundTasks, db: Session = Depends(session)):
     """ Runs A Pipeline. """
     run_options.pipeline_id = pipeline_id
@@ -47,17 +82,17 @@ def run_pipeline(pipeline_id: int, run_options: schemas.PipelineRunOptions, back
 
 # TODO: ask Adam why this didnt send the x and y coordinates... even tho they're defined in the model?
 # @router.get("/{pipeline_id}/nodes", response_model=List[schemas.PipelineNode])
-@router.get("/{pipeline_id}/nodes")
+@ router.get("/{pipeline_id}/nodes")
 def get_pipeline_nodes(pipeline_id: int, db: Session = Depends(session)):
     return db.query(PipelineNode).filter(PipelineNode.pipeline_id == pipeline_id).all()
 
 
-@router.get("/{pipeline_id}/links")
+@ router.get("/{pipeline_id}/links")
 def get_pipeline_links(pipeline_id: int, db: Session = Depends(session)):
     return db.query(PipelineLink).filter(PipelineLink.pipeline_id == pipeline_id).delete()
 
 
-@router.post("/{pipeline_id}", response_model=schemas.Pipeline)
+@ router.post("/{pipeline_id}", response_model=schemas.Pipeline)
 def update_pipeline(pipeline_id: int, pipeline_update: schemas.PipelineUpdate, db: Session = Depends(session)):
     """ This Allows you to update / add pipeline containers and links """
     print("pipeline payload from frontend")
@@ -88,6 +123,6 @@ def update_pipeline(pipeline_id: int, pipeline_update: schemas.PipelineUpdate, d
     return db.query(Pipeline).get(pipeline_id)
 
 
-@router.delete("/{pipeline_id}", response_model=schemas.Pipeline)
+@ router.delete("/{pipeline_id}", response_model=schemas.Pipeline)
 def delete_pipeline(pipeline_id: int, db: Session = Depends(session)):
     return db.query(Pipeline).get(pipeline_id).delete(db)
