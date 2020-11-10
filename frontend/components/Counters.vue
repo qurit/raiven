94a0a836920e8f245aa99294cef070f9d163656d
Adam <cadamrun@gmail.com>
2020-11-10 10:52:03 -0800

    Added beforeDestroy to clear the timer, updated to vuetify 2.3.1 syntax

--- frontend/components/Counters.vue
+++ frontend/components/Counters.vue
@@ -1,41 +1,47 @@
 <template>
-  <v-row class="d-flex justify-center">
-    <v-card
-      class="ma-1 text-center justify-center"
-      height="150"
-      width="175"
-      elevation="10"
-    >
-      <div
-        :class="`${fontsize} display-4 font-weight-light white--text`"
-        v-text="displayNumber"
-      />
-      <div class="title white--text" v-text="name" />
-    </v-card>
-  </v-row>
+  <v-card
+    class="ma-1 text-center"
+    height="150"
+    width="175"
+    elevation="10"
+  >
+    <v-row align="center">
+      <v-col>
+        <div
+          :class="`${fontsize} font-weight-light`"
+          v-text="displayNumber"
+        />
+        <div class="title white--text" v-text="name" style="position: relative; bottom: 0;"/>
+      </v-col>
+    </v-row>
+  </v-card>
+
 </template>
 
 <script>
 export default {
   name: 'Counters',
-  props: { number: { default: 0 }, name },
-  data: function() {
+  props: {
+    number: {type: Number, default: 0},
+    name: {type: String, default: ''},
+    speed: {type: Number, default: 30}
+  },
+  data: function () {
     return {
       displayNumber: 0,
       interval: false
     }
   },
-
   computed: {
     fontsize: ctx => {
       const length = ctx.displayNumber.toString().length
-      if (length <= 3) return 'display-4'
-      else if (length <= 5) return 'display-2'
-      else if (length <= 7) return 'display-1'
+      if (length <= 3) return 'text-h1'
+      else if (length <= 5) return 'text-h2'
+      else if (length <= 6) return 'text-h3'
+      else if (length <= 9) return 'text-h4'
       else return ''
     }
   },
-
   watch: {
     number: function() {
       clearInterval(this.interval)
@@ -53,9 +59,12 @@ export default {
           }
         }.bind(this),
         // this sets how quickly we increment to reach the displayNumber (higher number means longer delay between each number update)
-        30
+        this.speed
       )
     }
-  }
+  },
+  beforeDestroy() {
+    clearInterval(this.interval)
+  },
 }
 </script>
