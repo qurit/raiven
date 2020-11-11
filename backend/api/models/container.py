3280d75ba1e1b6365678d7ad703be2875393dbe9
Adam <cadamrun@gmail.com>
2020-11-11 11:09:38 -0800

    fixing tags

--- backend/api/models/container.py
+++ backend/api/models/container.py
@@ -41,7 +41,7 @@ class ContainerBuild(TimestampMixin, Base):
         return self.exit_code == 0
 
     def generate_tag(self) -> str:
-        return f'{config.IMAGE_TAG_PREFIX}.{self.container.name.strip()}.{self.container.id}'
+        return f'{config.IMAGE_TAG_PREFIX}.{self.container.name.strip()}.{self.container.id}'.replace(' ', '')
 
 
 class ContainerBuildError(Base):
