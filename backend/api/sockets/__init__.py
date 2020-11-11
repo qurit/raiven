84fa8a9e3344d53fedb50c5df7b0a080bae2e300
Adam <cadamrun@gmail.com>
2020-11-10 22:06:51 -0800

    merged with sockets branch

--- backend/api/sockets/__init__.py
+++ backend/api/sockets/__init__.py
@@ -1,6 +1,8 @@
 import socketio
 
-sio = socketio.AsyncServer(async_mode='asgi', cors_allowed_origins=[])
+mgr = socketio.AsyncAioPikaManager('amqp://')
+sio = socketio.AsyncServer(async_mode='asgi', cors_allowed_origins=[], client_manager=mgr)
 sio_app = socketio.ASGIApp(sio)
 
 from . import index
+
