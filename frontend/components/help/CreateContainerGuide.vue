54229ef675e8e90becb50ceb6dc103e5075b5129
Kevin <kevin.k.f.lam1@gmail.com>
2020-11-16 15:20:44 -0800

    fixed mobile stuff and a bit of the help guide

---
+++ frontend/components/help/CreateContainerGuide.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-card elevation="10">
+    <v-toolbar color="primary accent--text" flat>
+      <v-toolbar-title><b>Create Container Guide</b></v-toolbar-title>
+    </v-toolbar>
+    <v-divider light />
+    <v-card-subtitle>
+      <b>Creating a Container</b>
+    </v-card-subtitle>
+    <v-card-text>
+      <ol type="1">
+        <li>
+          Some stuff explaning what type of file stuff the user/researcher
+          should be uploading
+        </li>
+      </ol>
+    </v-card-text>
+  </v-card>
+</template>
