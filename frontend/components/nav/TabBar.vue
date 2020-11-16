d97179908de66aa7e7b1140a301429b6b2ea211a
Kevin <kevin.k.f.lam1@gmail.com>
2020-11-16 11:36:21 -0800

    fixed mobile logout tab bar

--- frontend/components/nav/TabBar.vue
+++ frontend/components/nav/TabBar.vue
@@ -1,23 +1,31 @@
 <template>
   <v-app-bar app flat class="px-0" color="#27272f">
-    <v-img class="mr-5" src="raiven-logo.svg" height="40" width="40" />
-    <v-tabs>
-      <v-tab v-for="link in items" :to="link.to" nuxt :ripple="false">
-        <v-icon
-          v-text="link.icon"
-          large
-          class="mx-auto"
-          :color="link.to === $route.path ? '#ffffff' : '#84848a'"
-        />
-        <v-slide-x-transition>
-          <div
-            v-if="link.to === $route.path"
-            v-text="link.label"
-            class="body-1"
-          />
-        </v-slide-x-transition>
-      </v-tab>
-    </v-tabs>
+    <n-link to="/" style="text-decoration: none">
+      <v-img
+        class="mr-5"
+        :src="require('@/static/raiven-logo.svg')"
+        height="40"
+        width="40"
+      />
+    </n-link>
+    <v-tab v-for="link in items" :to="link.to" nuxt :ripple="false">
+      <v-icon
+        v-text="link.icon"
+        large
+        class="mx-auto"
+        :color="link.to === $route.path ? '#ffffff' : '#84848a'"
+      />
+    </v-tab>
+    <v-btn
+      color="primary"
+      dark
+      icon
+      v-bind="attrs"
+      v-on="on"
+      @click="$auth.logout('local')"
+    >
+      <v-icon>mdi-power-standby</v-icon>
+    </v-btn>
   </v-app-bar>
 </template>
 
@@ -28,7 +36,7 @@ export default {
     items: {
       type: Array,
       default: () => [
-        {to: '/', label: 'Dashboard', icon: 'mdi-chart-box-outline'}
+        { to: '/', label: 'Dashboard', icon: 'mdi-chart-box-outline' }
       ]
     }
   }
