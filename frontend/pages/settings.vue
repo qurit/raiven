1a609bfd5c1e222d59d20c70ef69abf52438a25e
Kevin <kevin.k.f.lam1@gmail.com>
2020-11-18 16:35:38 -0800

    creating the user settings page

---
+++ frontend/pages/settings.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-row>
+      <v-col sm="12" md="5">
+        <UserSettings />
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import { UserSettings } from '~/components/user'
+
+export default {
+  components: { UserSettings }
+}
+</script>
