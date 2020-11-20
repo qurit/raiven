9bf4663ec41206853d0a62eef579cfc474a66d87
Kevin <kevin.k.f.lam1@gmail.com>
2020-11-20 11:19:30 -0800

    initial admin page

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,6 +1,5 @@
 <template>
   <v-app>
-
     <TabBar v-if="$vuetify.breakpoint.mdAndDown" :items="links" />
     <NavDrawer v-else app :items="links" />
     <Toast />
@@ -10,34 +9,41 @@
         <nuxt />
       </v-container>
     </v-content>
-
   </v-app>
 </template>
 <script>
-import NavDrawer from "../components/nav/NavDrawer";
-import Toast from "../components/generic/Toast";
-import TabBar from "../components/nav/TabBar";
+import NavDrawer from '../components/nav/NavDrawer'
+import Toast from '../components/generic/Toast'
+import TabBar from '../components/nav/TabBar'
 export default {
-  components: {TabBar, Toast, NavDrawer},
+  components: { TabBar, Toast, NavDrawer },
   computed: {
     links() {
       return [
-        {to: '/', label: 'Dashboard', icon: 'mdi-chart-box-outline'},
+        { to: '/', label: 'Dashboard', icon: 'mdi-chart-box-outline' },
         {
-          to: '/containers', label: 'Containers',
-          icon: this.$route.path === '/containers' ? 'mdi-package-variant' : 'mdi-package-variant-closed'
+          to: '/containers',
+          label: 'Containers',
+          icon:
+            this.$route.path === '/containers'
+              ? 'mdi-package-variant'
+              : 'mdi-package-variant-closed'
         },
         {
           to: '/pipeline',
           label: 'Pipelines',
           icon: 'mdi-transit-connection-variant'
         },
-        {to: '/runs', label: 'Runs', icon: 'mdi-air-filter'},
-        {to: '/help', label: 'Help', icon: 'mdi-help'}
+        { to: '/runs', label: 'Runs', icon: 'mdi-air-filter' },
+        { to: '/help', label: 'Help', icon: 'mdi-help' },
+        ...(this.$auth.user.is_admin
+          ? [{ to: '/admin', label: 'Admin', icon: 'mdi-brain' }]
+          : [])
       ]
     }
   },
   created() {
+    console.log(this.$auth.user)
     console.log(this.$vuetify.breakpoint)
   }
 }
---
+++ frontend/pages/admin.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <v-row>
+      <v-col cols="12">
+        <UserList />
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import { UserList } from '~/components/admin'
+export default {
+  components: { UserList }
+}
+</script>
