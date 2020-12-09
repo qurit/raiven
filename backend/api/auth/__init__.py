742f8445de3576bd6f01ce28b51108b13f647240
Adam <cadamrun@gmail.com>
2020-12-08 18:06:56 -0800

    Added admin_auth which allows routes to be set to admin access only

--- backend/api/auth/__init__.py
+++ backend/api/auth/__init__.py
@@ -10,10 +10,19 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
 
 def token_auth(token: str = Depends(oauth2_scheme), db: type(session) = Depends(session)):
     if not (user := User.verify_token(token, db)):
-        return HTTPException(401, "Invalid token")
+        raise HTTPException(401, "Invalid token")
     else:
         return user
 
 
 def socket_auth(token):
     return User.verify_token(token)
+
+
+def admin_auth(token: str = Depends(oauth2_scheme), db: type(session) = Depends(session)):
+    user = token_auth(token, db)
+
+    if type(user) is User and not user.is_admin:
+        raise HTTPException(403, "Unauthorized")
+
+    return user
