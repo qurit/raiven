118ccd7e5f68dfe58cefdd2231af4b1a76ec5a18
Adam <cadamrun@gmail.com>
2020-12-13 23:06:27 -0800

    moved user create to a better spot

--- backend/tests/utils.py
+++ backend/tests/utils.py
@@ -1,4 +1,17 @@
-from tests import models, config, TEST_USER
+from tests import models, config, schemas, TEST_USER, client
+
+
+def create_local_user(name, username, password) -> schemas.user.User:
+    user_to_create = schemas.user.UserLocalCreate(name=name, username=username, password=password)
+    response = client.post('/user/', json=user_to_create.dict())
+    data = response.json()
+
+    assert response.status_code == 200
+    assert data['username'] == user_to_create.username
+    assert data['name'] == user_to_create.name
+    assert not data['is_admin']
+
+    return schemas.user.User(**data)
 
 
 def get_test_user(db) -> models.user.User:
