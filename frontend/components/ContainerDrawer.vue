6b9f3a6774582ca6ab2101e26d60e5e4a94e2d9f
Kevin <kevin.k.f.lam1@gmail.com>
2020-12-13 23:54:26 -0800

    nav drawer own component

---
+++ frontend/components/ContainerDrawer.vue
@@ -0,0 +1,75 @@
+<template>
+  <v-navigation-drawer class="pt-3" app right style="z-index: 9999">
+    <template v-slot:prepend>
+      <v-row no-gutters justify="center" class="px-2">
+        <v-btn
+          @click="$emit('open-container-form')"
+          color="primary accent--text"
+          style="width: available"
+          class="mx-auto"
+          rounded
+          block
+        >
+          Add a Container
+          <v-icon>mdi-plus</v-icon>
+        </v-btn>
+      </v-row>
+      <v-row no-gutters class="pt-2 px-2">
+        <v-text-field
+          v-model="search"
+          placeholder="Search container"
+          append-icon="mdi-magnify"
+          solo
+          flat
+          rounded
+          block
+          color="primary"
+          single-line
+          hide-details
+        />
+      </v-row>
+    </template>
+    <ContainerCard
+      v-for="c in filteredList"
+      :id="c.id"
+      :container="c"
+      :colors="colors.container"
+      class="ma-2"
+    >
+      <v-icon-btn add @click="$emit('add-node', c)" color="white" />
+    </ContainerCard>
+  </v-navigation-drawer>
+</template>
+
+<script>
+import { ContainerForm, ContainerCard } from '~/components/container'
+
+export default {
+  components: {
+    ContainerCard
+  },
+  props: {
+    containers: Array
+  },
+  data() {
+    return {
+      colors: {
+        container: {
+          input: 'orange',
+          output: 'purple',
+          default: 'blue'
+        }
+      },
+      search: '',
+      containerList: false
+    }
+  },
+  computed: {
+    filteredList() {
+      return this.containers.filter(container =>
+        container.name.toLowerCase().includes(this.search.toLowerCase())
+      )
+    }
+  }
+}
+</script>
