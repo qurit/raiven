994ef9323018ecd8e48b8fa93e71a920ee31ce14
Adam <cadamrun@gmail.com>
2020-12-15 15:21:09 -0800

    Updated user tests

--- backend/tests/test_routes/test_auth.py
+++ backend/tests/test_routes/test_auth.py
@@ -1,4 +1,8 @@
-from tests import client, utils
+from tests import client, utils, TEST_USER, config
+
+
+def create_header(token):
+    return {'Authorization': token}
 
 
 def test_login():
@@ -48,5 +52,34 @@ def test_login_empty():
 def test_login_no_body():
     response = client.post('/auth/token')
     assert response.status_code == 422
+
+
+def test_invalid_token(authorization_header):
+    response = client.get('/user/me', headers=authorization_header)
+    assert response.status_code == 200
+
+    response = client.get('/user/me', headers=create_header('broken_token'))
+    assert response.status_code == 401
+
+    response = client.get('/user/me', headers=create_header('bearer broken_token'))
+    assert response.status_code == 401
+
+
+def test_expired_token(db):
+    from time import sleep
+    from api.models.user import User
+
+    test_user = utils.get_test_user(db)
+    token = test_user.generate_token()
+
+    assert token
+    assert User.verify_token(token)
+
+    User._set_serializer('my key', 0)
+    token = test_user.generate_token()
+    assert token
+
+    sleep(1)
+    assert not User.verify_token(token)
 
 
