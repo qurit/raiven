1712ff01f36ebd9456ac997f9d2f1e2c78bf9ec5
Adam <cadamrun@gmail.com>
2020-12-15 21:14:41 -0800

    fixing test_job_volumes_paths

--- backend/tests/__init__.py
+++ backend/tests/__init__.py
@@ -8,6 +8,7 @@ from sqlalchemy.orm import sessionmaker
 os.environ['UNIT_TESTING'] = 'True'
 os.environ['SQLALCHEMY_DATABASE_URI'] = "sqlite:///./test.db?check_same_thread=False"
 os.environ['UPLOAD_DIR'] = os.path.join(os.path.dirname(__file__), '.test_uploads')
+os.environ['UPLOAD_VOLUME_ABSPATH'] = os.path.join(os.path.dirname(__file__), '.test_uploads')
 os.environ['SCP_PORT'] = '11122'
 
 from api import app, models, config, schemas
