68221d69468308031db511ee135371caf38fd3cf
Kevin <kevin.k.f.lam1@gmail.com>
2020-12-18 01:59:07 -0800

    docker and main py

--- frontend/components/help/CreateContainerGuide.vue
+++ frontend/components/help/CreateContainerGuide.vue
@@ -8,22 +8,65 @@
     </v-card-subtitle>
     <v-card-text>
       <ol type="1">
-        <!-- TODO: Add text here -->
         <li>
-          When creating a container you must...
+          When creating a container, you should have a <b> Dockerfile </b> and
+          your <b> main algorithm </b>
+        </li>
+        <li>
+          The <b> Dockerfile </b> should resemble this example.
+          <ul>
+            <li>
+              Install any dependencies you may require and set the environment
+              variables to be <b> PICOM_INPUT_DIR /mnt/picom/input </b> and
+              <b> PICOM_OUTPUT_DIR /mnt/picom/output</b>.
+            </li>
+          </ul>
+          <ul>
+            <li>
+              Finally, write the instruction to run your
+              <b> main algorithm </b> code.
+            </li>
+          </ul>
+        </li>
+        <li>
+          The <b> main algorithm </b> should resemble this example.
+          <ul>
+            <li>
+              Map the <b> INPUT_DIR </b> to the previously defined
+              <b> PICOM_INPUT_DIR </b> and the <b> OUTPUT_DIR </b> to the
+              previously defined <b> PICOM_OUTPUT_DIR </b>
+              <ul>
+                <li>
+                  Alternatively, if you did not set up the
+                  <b> ENV </b> variables in the <b> Dockerfile </b>, then you
+                  can set the <b> INPUT_DIR </b> to be
+                  <b> /mnt/picom/input </b> and the <b> OUTPUT_DIR </b> to be
+                  <b> /mnt/picom/output </b>.
+                </li>
+              </ul>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              Insert your DICOM processing algorithm.
+            </li>
+          </ul>
+        </li>
+        <li>
+          Place both the <b> Dockerfile </b> and <b> main algorithm </b> in a
+          zippped folder. You are now ready to move onto the next step,
+          <b> Uploading a Container</b>!
         </li>
       </ol>
     </v-card-text>
-    <v-card-subtitle>
-      <b>Uploading a Container</b>
-    </v-card-subtitle>
-    <v-card-text>
-      <ol type="1">
-        <!-- TODO: Add text here       -->
-        <li>
-          When creating a container you must ...
-        </li>
-      </ol>
-    </v-card-text>
+    <v-dialog>
+      <v-card>
+        <v-card-text>
+          FROM python:3.8-slim WORKDIR /src RUN pip install pydicom numpy COPY .
+          . ENV PICOM_INPUT_DIR /mnt/picom/input ENV PICOM_OUTPUT_DIR
+          /mnt/picom/output CMD python main.py
+        </v-card-text>
+      </v-card>
+    </v-dialog>
   </v-card>
 </template>
