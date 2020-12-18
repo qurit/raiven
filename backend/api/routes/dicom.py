dc92a156ca9074a512d7787afbe94d5f4b4726ed
Kevin <kevin.k.f.lam1@gmail.com>
2020-12-18 12:12:34 -0800

    sdfg

--- backend/api/routes/dicom.py
+++ backend/api/routes/dicom.py
@@ -92,3 +92,31 @@ def delete_study(study_id: int, db: Session = Depends(session)):
 def delete_series(series_id: int, db: Session = Depends(session)):
     """ Delete a series """
     return db.query(DicomSeries).get(series_id).delete(db)
+
+# the send to container code stuff
+
+
+@ router.put("/node/{dicom_node_id}")
+def send_dicom_node(dicom_node_id: int, pipeline_id: pipeline.PipelineId, db: Session = Depends(session)):
+    print(dicom_node_id)
+    print(pipeline_id)
+    dicom_node = db.query(DicomNode).get(dicom_node_id)
+    return dicom_node.abs_path
+
+
+@ router.put("/node/{dicom_node_id}/{dicom_patient_id}")
+def send_dicom_patient(dicom_node_id: int, dicom_patient_id: int, pipeline_id: pipeline.PipelineId, db: Session = Depends(session)):
+    dicom_patient = db.query(DicomPatient).get(dicom_patient_id)
+    return dicom_patient.abs_path
+
+
+@ router.put("/node/{dicom_node_id}/{dicom_patient_id}/{dicom_study_id}")
+def send_dicom_study(dicom_node_id: int, dicom_patient_id: int, dicom_study_id: int, pipeline_id: pipeline.PipelineId, db: Session = Depends(session)):
+    dicom_study = db.query(DicomStudy).get(dicom_study_id)
+    return dicom_study.abs_path
+
+
+@ router.put("/node/{dicom_node_id}/{dicom_patient_id}/{dicom_study_id}/{dicom_series_id}")
+def send_dicom_series(dicom_node_id: int, dicom_patient_id: int, dicom_study_id: int, dicom_series_id: int, pipeline_id: pipeline.PipelineId, db: Session = Depends(session)):
+    dicom_series = db.query(DicomSeries).get(dicom_series_id)
+    return dicom_series.abs_path
