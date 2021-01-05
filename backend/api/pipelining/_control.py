d7d256a68a957c316501f0b166b3068e17913f2f
Adam <cadamrun@gmail.com>
2021-01-04 17:38:35 -0800

    Tests passing. New SCP backend is done w/ some fancy testing

--- backend/api/pipelining/_control.py
+++ backend/api/pipelining/_control.py
@@ -1,10 +1,11 @@
+import pathlib
 from os import PathLike
 from typing import Union
 
-from api.models import pipeline as models, utils
-from api.schemas import pipeline as schemas
-
+from api import config
+from api.models import utils
 from api.models.pipeline import PipelineRun
+
 from ._tasks import build, run, test, ingest
 
 
@@ -42,13 +43,15 @@ class PipelineController:
 class DicomIngestController:
 
     @staticmethod
-    def ingest_folder(folder: Union[str, PathLike], calling_aet: str, calling_host: str, calling_port: int, called_aet: str):
+    def ingest_folder(folder: pathlib.Path, calling_aet: str, calling_host: str, calling_port: int, called_aet: str):
+        rel_folder: str = folder.relative_to(config.UPLOAD_DIR).as_posix()
+
         # TODO: Finish logic here.
 
         # Pushed globally
         if called_aet == config.SCP_AE_TITLE:
-            args = (folder, calling_aet, calling_host, calling_port)
-            ingest.run_ingest_task.send(args=args)
+            args = (rel_folder, calling_aet, calling_host, calling_port)
+            ingest.run_ingest_task.send_with_options(args=args)
 
         # Pushed to user
         elif called_aet.startswith(config.USER_AE_PREFIX):
