c50597124e4f69505683100a6d1d76b2f73b5438
Adam <cadamrun@gmail.com>
2021-01-08 11:36:13 -0800

    whitespace update

--- backend/tests/test_config.py
+++ backend/tests/test_config.py
@@ -6,4 +6,3 @@ def test_config():
     assert os.path.exists(config.UPLOAD_DIR)
     assert config.SQLALCHEMY_DATABASE_URI == os.environ['SQLALCHEMY_DATABASE_URI']
     assert config.UPLOAD_DIR == os.environ['UPLOAD_DIR']
-
