b45637d218dc05694263683b7319b982f9245ece
Adam <cadamrun@gmail.com>
2021-01-12 12:07:18 -0800

    ✅ Updated Tests

--- backend/tests/test_pipelining/test_ingest.py
+++ backend/tests/test_pipelining/test_ingest.py
@@ -10,17 +10,13 @@ from tests.test_models.test_pipelines import insert_pipeline
 def test_ingest_with_bad_ae_title(mock_images_folder):
     # TODO update test when edge case is implemented
     with pytest.raises(NotImplementedError):
-        DicomIngestController.ingest_folder (
-            mock_images_folder, "test", "test", "0", "BAD_AET"
-            )
+        DicomIngestController(mock_images_folder, "test", "test", "0", "BAD_AET")
 
 
 def test_ingest_folder_to_bad_pipeline(mock_images_folder):
     # TODO update test when edge case is implemented
     with pytest.raises(NotImplementedError):
-        assert not DicomIngestController.ingest_folder (
-            mock_images_folder, "test", "test", "0", config.PIPELINE_AE_PREFIX + "non-existant-ae"
-            )
+        assert not DicomIngestController(mock_images_folder, "test", "test", "0", config.PIPELINE_AE_PREFIX + "non-existant-ae")
 
 
 def test_ingest_folder_to_valid_pipeline(mock_images_folder, db):
@@ -30,9 +26,7 @@ def test_ingest_folder_to_valid_pipeline(mock_images_folder, db):
 
     # Test the pipeline was run
     init_pipeline_run_count = db.query(PipelineRun).count()
-    assert DicomIngestController.ingest_folder (
-        mock_images_folder, "test", "test", "0", config.PIPELINE_AE_PREFIX + ae_title
-        )
+    assert DicomIngestController(mock_images_folder, "test", "test", "0", config.PIPELINE_AE_PREFIX + ae_title)
     assert init_pipeline_run_count < db.query(PipelineRun).count()
     assert not os.path.exists(mock_images_folder.resolve()) # Temp folder gets removed on delete
     
