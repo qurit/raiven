0cf9620fa13ec3b2c4f67dc4bba3d59b6d3d8c36
Asim Shrestha <asim.shrestha@hotmail.com>
2021-01-13 12:10:44 -0800

    feat: add test for user dicom node route

---
+++ backend/tests/test_routes/test_dicom.py
@@ -0,0 +1,38 @@
+from sqlalchemy.orm.session import Session
+from tests import client, utils
+from api.models.dicom import DicomNode
+
+def test_get_user_dicom_nodes(db, authorization_header):
+    # Delete existing DICOM nodes in db
+    db.query(DicomNode).delete()
+    db.commit()
+
+    current_user = utils.get_test_user(db)
+    other_user = utils.create_local_user("other", "other", "other")
+
+    # Create DICOM
+    global_node = save_node_for_user_id(None, db)
+    user_node = save_node_for_user_id(current_user.id, db)
+    unauthorized_node = save_node_for_user_id(other_user.id, db)
+    db.commit()
+
+    response = client.get(f'/dicom/nodes/{current_user.id}', headers=authorization_header)
+    data = response.json()
+
+    assert response.status_code == 200
+    assert len(data) == 2
+    for node in data:
+        assert node['user_id'] in [global_node.user_id, user_node.user_id]
+
+def test_get_other_user_dicom_nodes(authorization_header):
+    other_user = utils.create_local_user("private", "private", "private")
+
+    response = client.get(f'/dicom/nodes/{other_user.id}', headers=authorization_header)
+
+    assert response.status_code == 401
+
+
+def save_node_for_user_id(user_id: int, db: Session):
+    node = DicomNode(title='test', host='test', port='0000', user_id=user_id)
+    node.save(db)
+    return node
