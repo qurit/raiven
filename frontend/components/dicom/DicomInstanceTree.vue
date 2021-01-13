a4b2a07c7a58517aec2202c4160fb1fb77ea2974
Asim Shrestha <asim.shrestha@hotmail.com>
2021-01-13 12:50:20 -0800

    feat: initial front end changes to display global and private

---
+++ frontend/components/dicom/DicomInstanceTree.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-treeview
+    dense
+    :items="nodes"
+    item-text="id"
+    :loadChildren="loadChildren"
+    hoverable
+  >
+    <template v-slot:prepend="{ item }">
+      <v-icon v-if="item.icon" v-text="item.icon"></v-icon>
+    </template>
+
+    <template slot="label" slot-scope="{ item }">
+      <a v-if="item.hasOwnProperty('host')" @click="send('Node', item.id)">
+        {{ item.title }}
+        <span class="text-caption"
+          >Host: {{ item.host }} Port: {{ item.port }}</span
+        >
+      </a>
+      <a
+        v-else-if="item.hasOwnProperty('patient_id')"
+        @click="send('Patient', item.id)"
+      >
+        {{ item.patient_id }}
+      </a>
+      <a
+        v-else-if="item.hasOwnProperty('study_instance_uid')"
+        @click="send('Study', item.id)"
+      >
+        {{ new Date(item.study_date).toLocaleDateString() }}
+      </a>
+      <a v-else @click="send('Series', item.id)">
+        {{ item.series_description }}
+      </a>
+    </template>
+  </v-treeview>
+</template>
+
+<script>
+export default {
+  name: 'DicomInstanceTree',
+  props: ['nodes', 'loadChildren', 'send'],
+}
+</script>
