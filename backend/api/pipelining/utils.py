81788398bf18476b50ae088774f511315b5ee97c
Adam <cadamrun@gmail.com>
2021-01-20 00:16:48 -0800

    :lipstick: Setup Error Response From Backend and Removed old validation

--- backend/api/pipelining/utils.py
+++ backend/api/pipelining/utils.py
@@ -7,13 +7,13 @@ from networkx.algorithms.components import is_connected
 
 
 def validate_pipeline_structure(graph: Union[Graph, DiGraph]) -> bool:
-    assert is_connected(graph.to_undirected()), 'The pipeline must be connected'
+    assert is_connected(graph.to_undirected()), 'The pipeline must be connected.'
 
     try:
         find_cycle(graph)
     except NetworkXNoCycle:
         pass
     else:
-        raise ValueError('The pipeline must be connected')
+        raise ValueError('The pipeline must not contain a cycle.')
 
     return True
