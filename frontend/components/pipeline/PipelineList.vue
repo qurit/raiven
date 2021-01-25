8e269bfc6c65cc67b343779dffeb5a69e95b9ec5
Kevin <kevin.k.f.lam1@gmail.com>
2021-01-24 17:07:40 -0800

    added confirmation for delete pipeline

--- frontend/components/pipeline/PipelineList.vue
+++ frontend/components/pipeline/PipelineList.vue
@@ -17,7 +17,9 @@
         solo
       />
       <v-btn icon>
-        <v-icon @click="dialog = true" large color="#373740">mdi-plus</v-icon>
+        <v-icon @click="addPipelineDialog = true" large color="#373740"
+          >mdi-plus</v-icon
+        >
       </v-btn>
     </v-toolbar>
     <v-data-table
@@ -32,13 +34,35 @@
         <v-simple-checkbox :value="item.is_shared" disabled />
       </template>
       <template v-slot:item.actions="{ item }">
-        <v-icon medium @click.stop="removePipeline(item)" color="cancel">
+        <v-icon medium @click.stop="deletePipeline(item.id)" color="cancel">
           mdi-delete
         </v-icon>
       </template>
     </v-data-table>
-    <v-dialog v-model="dialog" max-width="600px">
-      <AddPipelineForm @closeDialog="dialog = false" />
+    <v-dialog v-model="addPipelineDialog" max-width="600px">
+      <AddPipelineForm @closeDialog="addPipelineForm = false" />
+    </v-dialog>
+    <v-dialog
+      v-model="confirmDeleteDialog"
+      max-width="525px"
+      min-height="600px"
+    >
+      <v-card class="overflow-x-hidden">
+        <v-card-title style="word-break: break-word">
+          Deleting this pipeline will clear all of its run results as well.
+        </v-card-title>
+        <v-card-subtitle>
+          Are you sure you want to continue?
+        </v-card-subtitle>
+        <v-card-actions class="justify-center">
+          <v-btn text color="confirm" @click="confirmDeletePipeline">
+            Yes
+          </v-btn>
+          <v-btn text color="cancel" @click="confirmDeleteDialog = false">
+            No
+          </v-btn>
+        </v-card-actions>
+      </v-card>
     </v-dialog>
   </v-card>
 </template>
@@ -53,7 +77,9 @@ export default {
   },
   data: function() {
     return {
-      dialog: false,
+      addPipelineDialog: false,
+      confirmDeleteDialog: false,
+      deletePipelineId: null,
       headers: [
         { text: 'Pipeline Name', value: 'name' },
         { text: 'AE Title', value: 'ae_title' },
@@ -72,13 +98,21 @@ export default {
     viewPipeline(pipeline) {
       this.$router.push({ path: `/pipeline/${pipeline.id}` })
     },
-    async removePipeline(pipeline) {
+    async confirmDeletePipeline() {
       try {
-        await this.$store.dispatch('pipelines/deletePipeline', pipeline.id)
+        await this.$store.dispatch(
+          'pipelines/deletePipeline',
+          this.deletePipelineId
+        )
         this.$toaster.toastSuccess('Pipeline deleted!')
       } catch (e) {
         this.$toaster.toastError('Could not delete pipeline!')
       }
+      this.confirmDeleteDialog = false
+    },
+    deletePipeline(pipelineId) {
+      this.deletePipelineId = pipelineId
+      this.confirmDeleteDialog = true
     }
   },
   computed: {
