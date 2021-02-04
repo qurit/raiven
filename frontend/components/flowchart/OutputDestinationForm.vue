e557230727b707486bd53d756dba0c0415edc1d4
Adam <cadamrun@gmail.com>
2021-02-04 15:38:48 -0800

    :building_construction: Added form validation rules

--- frontend/components/flowchart/OutputDestinationForm.vue
+++ frontend/components/flowchart/OutputDestinationForm.vue
@@ -1,50 +1,55 @@
 <template>
-   <v-card dense max-width="400">
-    <v-toolbar color="primary accent--text" dense flat>{{ title }}</v-toolbar>
-     <v-card-text>
-       <v-form v-model="isFormValid">
-         <v-row>
-           <v-col cols="6">
-             <v-text-field
-               v-bind="textFieldAttrs"
-               v-model="node.host"
-               label="Host"
-             />
-           </v-col>
-           <v-col cols="6">
-             <v-text-field
-               v-bind="textFieldAttrs"
-               v-model="node.port"
-               label="Port"
-               type="number"
-             />
-           </v-col>
-           <v-col cols="12">
-             <v-text-field
-               v-bind="textFieldAttrs"
-               v-model="node.title"
-               label="AE Title"
-             />
-           </v-col>
-         </v-row>
-       </v-form>
-     </v-card-text>
+  <v-card dense max-width="400">
+    <v-toolbar :color="color" dense flat>{{ title }}</v-toolbar>
+    <v-card-text>
+      <v-form v-model="isFormValid" ref="form" lazy-validation>
+        <v-row>
+          <v-col cols="6">
+            <v-text-field
+              v-bind="textFieldAttrs"
+              v-model="node.host"
+              :rules="[validateHostAddress]"
+              color="red"
+              label="Host"
+            />
+          </v-col>
+          <v-col cols="6">
+            <v-text-field
+              v-bind="textFieldAttrs"
+              v-model="node.port"
+              :rules="[validatePort]"
+              label="Port"
+              type="number"
+            />
+          </v-col>
+          <v-col cols="12">
+            <v-text-field
+              v-bind="textFieldAttrs"
+              v-model="node.title"
+              :rules="[validateNotEmpty]"
+              label="AE Title"
+            />
+          </v-col>
+        </v-row>
+      </v-form>
+    </v-card-text>
     <v-card-actions>
-      <v-spacer />
-      <v-btn rounded dense small text v-text="'Cancel'" @click="$emit('close')" />
-      <v-btn rounded color="primary accent--text" v-text="'Save'" small @click="submit" />
+      <v-spacer/>
+      <v-btn rounded dense small text v-text="'Cancel'" @click="$emit('close')"/>
+      <v-btn rounded :color="color" v-text="'Save'" small @click="submit"/>
     </v-card-actions>
   </v-card>
 </template>
 
 <script>
 import { toPropFormat } from "@/utilities/propHelpers";
+import { validateHostAddress, validatePort, validateNotEmpty } from '~/utilities/validationRules'
 
 export default {
   name: "OutputDestinationForm",
   data: () => ({
     title: 'Add Dicom Node',
-    isFormValid: false,
+    isFormValid: true,
     textFieldAttrs: toPropFormat(['solo', 'single-line', 'hide-details', 'dense', 'flat']),
     node: {
       title: undefined,
@@ -53,13 +58,16 @@ export default {
     }
   }),
   methods: {
+    validateHostAddress,
+    validatePort,
+    validateNotEmpty,
+
     async submit() {
-      await this.$store.dispatch('destination/addDestination', this.node)
-      this.$emit('close')
+      this.isFormValid = this.$refs.form.validate()
     }
   },
   computed: {
-    isDisabled: ctx => !ctx.host || !ctx.port || !ctx.ae_title
+    color: ({isFormValid}) => isFormValid ? 'primary accent--text' : 'error'
   }
 }
 </script>
