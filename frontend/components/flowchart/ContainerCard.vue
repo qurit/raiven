7533054dfa0166669e7a234adb602d71d4309a05
Kevin <kevin.k.f.lam1@gmail.com>
2021-02-22 10:17:36 -0800

    container card show tags

--- frontend/components/flowchart/ContainerCard.vue
+++ frontend/components/flowchart/ContainerCard.vue
@@ -11,24 +11,30 @@
         {{ container.description }}
       </v-card-subtitle>
       <v-row justify="center" v-if="container.user_id !== $auth.user.id">
-        <v-chip class="mb-1" v-text="'From: ' + container.user.name" color="info" />
+        <v-chip
+          class="mb-1"
+          v-text="'From: ' + container.user.name"
+          color="info"
+        />
       </v-row>
-      <v-card-actions class="mt-n2">
+      <v-row class="ma-1">
         <v-chip
           v-for="chip in chips"
           v-text="chip"
-          class="mr-1"
-          :color="containerColor"
+          class="mr-1 mb-1"
+          color="confirm"
         />
         <v-spacer />
         <!-- Slot to enable adding of custom actions -->
         <slot></slot>
-      </v-card-actions>
+      </v-row>
     </v-card>
   </v-hover>
 </template>
 <script>
+import vIconBtn from '../global/v-icon-btn.vue'
 export default {
+  components: { vIconBtn },
   name: 'ContainerCard',
   props: {
     container: {
@@ -38,23 +44,17 @@ export default {
     colors: {
       type: Object,
       default: () => ({
-        input: 'orange',
-        output: 'purple',
-        default: 'blue'
+        default: 'blue',
+        tag: 'green'
       })
     }
   },
   computed: {
-    containerColor: ctx => {
-      if (ctx.container.is_output_container) return ctx.colors.output
-      else if (ctx.container.is_input_container) return ctx.colors.input
-      else return ctx.colors.default
-    },
     chips: ctx => {
-      const chips = []
-      if (ctx.container.is_output_container) chips.push('Output')
-      if (ctx.container.is_input_container) chips.push('Input')
-      return chips
+      if (!!ctx.container.tags) {
+        const chips = ctx.container.tags.split(',')
+        return chips
+      }
     }
   }
 }
