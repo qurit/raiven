6b00869c4f3da77c139f8ffa97e4a6a0521da0f6
Kevin <kevin.k.f.lam1@gmail.com>
2021-04-20 18:42:58 -0700

    fix user change own ae title

--- backend/api/routes/user.py
+++ backend/api/routes/user.py
@@ -54,8 +54,11 @@ def edit_user_settings(user_id: int, new_info: UserEdit, user: User = Depends(to
         return HTTPException(401, 'User does not exist')
 
     user_to_edit.ae_title = new_info.ae_title
-    user_to_edit.access_allowed = new_info.access_allowed
-    user_to_edit.is_admin = new_info.is_admin
+
+    if (user.is_admin):
+        user_to_edit.access_allowed = new_info.access_allowed
+        user_to_edit.is_admin = new_info.is_admin
+
     user_to_edit.save(db)
     return user_to_edit
 
